bb8055a1fac4d1a2f34c6bb505d66a10c927654a
youjin Cho <ud071233@gmail.com>
2025-05-05 23:08:33 +0900

    feat: core 앱 생성 및 모델, URL 작성, 마이그레이션 적용

--- netflixclone/settings.py
+++ netflixclone/settings.py
@@ -19,7 +19,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -42,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -129,6 +129,9 @@ MEDIA_URL="/media/"
 STATICFILES_DIRS=[
     BASE_DIR/"static"
 ]
+
+# Auth setting
+AUTH_USER_MODEL="core.CustomUser"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
