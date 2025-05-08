c9b3b878a1b5ef5c2ba904f97b5a0c0cac5f7af5
유진 <ud071233@gmail.com>
2025-05-08 17:46:55 +0900

    day4

--- core/views.py
+++ core/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 class Home(View):
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
         return render(request, "index.html")
+
+@method_decorator(login_required, name="dispatch")    
+class ProfildList(View):
+    def get(self, request):
+        profiles = request.user.profiles.all()
+        print(profiles)
+        return render(request, "profileList.html", {
+            "profiles": profiles
+        })
